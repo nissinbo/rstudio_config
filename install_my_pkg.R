7b53dcf1589d688d8a0926e8da8d0e00211d2975
nissinbo <43395303+nissinbo@users.noreply.github.com>
2022-04-15 11:20:26 +0900

    add instll_my_pkg

---
+++ install_my_pkg.R
@@ -0,0 +1,27 @@
+install_my_pkg <- function(minimum = TRUE, recommend = FALSE, 
+                   option = FALSE, ubuntu = FALSE) {
+  pkglist <- NULL
+  
+  if (ubuntu) {
+    system("sudo hoge")
+  }
+  
+  if (minimum) {
+    pkglist <- c(pkglist, "tidyverse", "remotes", "devtools", "installr", "fs", "here", "pacman", "openxlsx", "data.table", "knitr", "rmarkdown", "lubridate", "styler", "renv")
+  }
+  
+  if (recommend) {
+    pkglist <- c(pkglist, "patchwork", "gt", "gtsummary", "flextable", "ftExtra", "xaringan")
+  }
+  
+  if (option) {
+    pkglist <- c(pkglist, "BiocManager", "tidymodels", "survival", "survminer", "Shiny", "rsconnect", "shinycssloaders", "rvest", "datapasta", "comorbidity", "touch", "AdhereR", "ggdag", "dagitty", "reticulate")
+    pkglist_github <- c("cran/icd", "MilesMcBain/breakerofchains", "tgerke/ggconsort")
+  }
+  
+  install.packages(pkglist, dependencies = TRUE)
+  remotes::install_github(pkglist_github)
+}
+
+install_my_pkg(minimum = TRUE, recommend = FALSE, 
+               option = FALSE, ubuntu = FALSE))
