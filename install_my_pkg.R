1034cc73a706230d6970f99f393cfb1af32f3b68
nissinbo <43395303+nissinbo@users.noreply.github.com>
2022-04-25 17:05:28 +0900

    Update install_my_pkg.R

--- install_my_pkg.R
+++ install_my_pkg.R
@@ -4,7 +4,31 @@ install_my_pkg <- function(minimum = TRUE, recommend = FALSE,
   pkglist_github <- NULL
   
   if (ubuntu) {
-    system("sudo hoge")
+    system("sudo apt update
+  sudo apt install -y gdebi-core wget
+  sudo apt install -y fonts-ipaexfont
+  fc-cache -f -v
+  fc-list | grep IPAex
+  sudo apt install -y build-essential
+  sudo apt install -y libcurl4-openssl-dev
+  sudo apt install -y libcurl4-gnutls-dev
+  sudo apt install -y libxml2-dev
+  sudo apt install -y libssl-dev
+  sudo apt install -y libx11-dev
+  sudo apt install -y libglu1-mesa-dev
+  sudo apt install -y libmagick++-dev
+  sudo apt install -y libudunits2-0
+  sudo apt install -y libudunits2-dev
+  sudo apt install -y libgdal-dev libproj-dev
+  sudo apt install -y libgmp3-dev
+  sudo apt install -y curl
+  sudo apt install -y fcitx-frontend-qt5
+  sudo ln -s /usr/lib/x86_64-linux-gnu/qt5/plugins/platforminputcontexts/libfcitxplatforminputcontextplugin.so /usr/lib/rstudio/plugins/platforminputcontexts/
+  sudo apt install -y libxml2-dev
+  sudo apt install -y libssl-dev
+  sudo apt install -y libcurl4-openssl-dev")
+
+  pkglist <- c(pkglist, "xml2", "httr", "cutl", "rvest")
   }
   
   if (minimum) {
